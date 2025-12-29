de69759e3e8a9c993f274d4671439aaa17c0f882
summ3rNight <tuanla50@icloud.com>
2025-12-29 23:05:59 +0100

    change gitignore

--- e1_binarySearch/src/test/BinSeaTest.java
+++ e1_binarySearch/src/test/BinSeaTest.java
@@ -1,11 +1,21 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
+import java.gad.binarysearch.*;
 
 public class BinSeaTest {
-    @Test
-    public void testBinarySearch() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        assertEquals(0, 0);
+    @BeforeAll
+    public static void setup() {
+        int[] normal_array = new int[] { 2, 7, 7, 42, 69, 1337, 2000, 9001 };
+        int[] empty_array = new int[] {};
+        int[] single_element_array = new int[] { 42 };
     }
+    @Test
+    public void testSearchFound() {
+
+    }
+
+
+
 }
